36705eb1769b84acd23d64919acc201259afb6fd
John Dempsey <v-jodemp@microsoft.com>
2017-10-02 14:27:48 -0700

    diceroll

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,63 +1,27 @@
-using System.Threading.Tasks;
-using System.Web.Http;
-
-using Microsoft.Bot.Connector;
-using Microsoft.Bot.Builder.Dialogs;
-using System.Web.Http.Description;
-using System.Net.Http;
-
-namespace Microsoft.Bot.Sample.SimpleEchoBot
+﻿namespace RollerSkillBot
 {
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+    using RollerSkillBot.Dialogs;
+
     [BotAuthentication]
     public class MessagesController : ApiController
     {
         /// <summary>
         /// POST: api/Messages
-        /// receive a message from a user and send replies
+        /// Receive a message from a user and reply to it
         /// </summary>
-        /// <param name="activity"></param>
-        [ResponseType(typeof(void))]
-        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            // check if activity is of type message
-            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
-            {
-                await Conversation.SendAsync(activity, () => new EchoDialog());
-            }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
-            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
-        }
+            await Conversation.SendAsync(activity, () => new RootDispatchDialog());
 
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
 
-            return null;
+            return response;
         }
     }
 }
